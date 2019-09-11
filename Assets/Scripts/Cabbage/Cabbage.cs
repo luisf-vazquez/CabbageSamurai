e6848a5d1504013179a4483b77e6684bde3f36c1
luisf-vazquez <luisvazquezcaracas@gmail.com>
2019-09-11 21:58:01 +0200

    First Upload

---
+++ Assets/Scripts/Cabbage/Cabbage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cabbage : MonoBehaviour
+{
+    
+    public bool isActive { get; set; }
+    private float verticalSpeed;
+    private float horizontalSpeed;
+    private float gravity = 2.0f;
+    private bool isCutted=false;
+    // Start is called before the first frame update
+    private void Start()
+    {
+    
+    }
+
+    public void LaunchCabbage(float ySpeed,float xSpeed,float xStart)
+    {
+        isActive = true;
+        verticalSpeed = ySpeed;
+        horizontalSpeed = xSpeed;
+        transform.position = new Vector3(xStart, 0, 0);
+    }
+
+    public void Update()
+    {
+        if (!isActive)
+        {
+            return;
+        }
+        verticalSpeed -= gravity * Time.deltaTime;
+        transform.position += new Vector3(horizontalSpeed, verticalSpeed, 0)* Time.deltaTime;
+
+        if(transform.position.y < -1)
+        {
+            isActive = false;
+        }
+    }
+
+    public void CutCabbage()
+    {
+        if (!isCutted)
+        {
+            if (verticalSpeed < 0.5f)
+            {
+                verticalSpeed = 0.5f;
+            }
+            horizontalSpeed = horizontalSpeed / 2;
+
+            isCutted = true;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Manager/Gamemanager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Cabbage> cabbages = new List<Cabbage>();
+
+    public GameObject cabbagePrefab;
+    private float lastSpawn;
+    private float deltaSpawn = 1.0f;
+    public Transform trail;
+    private Collider2D[] collsCabbages;
+    private Vector3 lastMousePos;
+
+    private const float  minimum_cut_Speed= 400.0f;
+    private Cabbage GetCabbage()
+    {
+        Cabbage c = cabbages.Find(x => !x.isActive);
+        if( c== null)
+        {
+            c = Instantiate(cabbagePrefab).GetComponent<Cabbage>();
+            cabbages.Add(c);
+        }
+
+        return c;
+    }
+    void Start()
+    {
+        collsCabbages = new Collider2D[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - lastSpawn > deltaSpawn)
+        {
+
+            Cabbage cabbage = GetCabbage();
+            float randomX = Random.Range(-1.55f, 1.55f);
+            float randomY = Random.Range(1.75f, 2.75f);
+            cabbage.LaunchCabbage(randomY, randomX, -randomX);
+            lastSpawn = Time.time;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            
+            Vector3 position= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            position.z = -1;
+            trail.position = position;
+
+            Collider2D[] thisFrameCabbages= Physics2D.OverlapPointAll(new Vector2(position.x, position.y), LayerMask.GetMask("Cabbage"));
+            
+            if((Input.mousePosition - lastMousePos).sqrMagnitude >= minimum_cut_Speed)
+            {
+                foreach (Collider2D c2 in thisFrameCabbages)
+                {
+                    for (int i = 0; i < collsCabbages.Length; i++)
+                    {
+                        if (c2 == collsCabbages[i])
+                        {
+                            Debug.Log("Sliced " + c2.name);
+                            c2.GetComponent<Cabbage>().CutCabbage();
+                        }
+                    }
+                }
+            }
+            lastMousePos = Input.mousePosition;
+            collsCabbages = thisFrameCabbages;
+        }
+    }
+}
